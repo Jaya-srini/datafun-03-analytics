cd3f1c9c62d1ca8f4e697e7c5c164ca03775e5bf
Jaya <jayasrini139@gmail.com>
2024-01-26 21:13:44 -0600

    Final commit - Project completed

--- jaya_analytics.py
+++ jaya_analytics.py
@@ -13,6 +13,8 @@ from pathlib import Path
 # External library imports (requires virtual environment)
 import requests
 import json
+import io
+import pandas as pd
 
 # Local module imports
 import jaya_projsetup      
@@ -84,6 +86,71 @@ def fetch_and_write_json_data(folder_name, filename, url):
     except requests.RequestException as e:
         print(f"Error fetching JSON data: {e}")
 
+# Function - Process text data  
+def process_txt_file(folder_name, input_filename, output_filename):
+    # Fetch the data
+    txt_url = 'https://txt2html.sourceforge.net/sample.txt'
+    response = requests.get(txt_url)
+    
+    if response.status_code == 200:
+        data = response.text
+        write_txt_file(folder_name, input_filename, data)
+        processed_data = data  
+        write_txt_file(folder_name, output_filename, processed_data)
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+
+# Function - Process CSV data
+def process_csv_file(csv_folder_name, input_filename, output_filename):
+    # Fetch the data
+    csv_url = 'https://media.githubusercontent.com/media/datablist/sample-csv-files/main/files/customers/customers-100.csv'
+    response = requests.get(csv_url)
+    
+    if response.status_code == 200:
+        data = response.text
+        write_csv_file(csv_folder_name, input_filename, data)
+        processed_data = data 
+        write_csv_file(csv_folder_name, output_filename, processed_data)
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+
+# Function - Process Excel data
+def process_excel_file(excel_folder_name, input_filename, output_filename):
+    # Fetch the data
+    excel_url = 'https://github.com/bharathirajatut/sample-excel-dataset/raw/master/cattle.xls'
+    response = requests.get(excel_url)
+    
+    if response.status_code == 200:
+        data = response.content
+        write_excel_file(excel_folder_name, input_filename, data)
+
+        # Use pandas to read Excel data directly from binary data
+        excel_data = pd.read_excel(io.BytesIO(data), sheet_name='Sheet1')  # Update 'Sheet1' to the actual sheet name
+
+        # Convert the read Excel data to CSV format
+        processed_data = excel_data.to_csv(index=False)
+        
+        # Write the processed data to a CSV file
+        csv_output_filename = output_filename.replace('.xls', '.csv')
+        write_csv_file(excel_folder_name, csv_output_filename, processed_data)
+        
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+
+# Function - Process JSON data
+def process_json_file(json_folder_name, input_filename, output_filename):
+    # Fetch the data
+    json_url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json'
+    response = requests.get(json_url)
+    
+    if response.status_code == 200:
+        data = response.content
+        write_json_file(json_folder_name, input_filename, data)
+        write_json_file(json_folder_name, output_filename, data)
+       
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+
 #Function - main
 def main():
     '''
@@ -97,7 +164,7 @@ def main():
     url_text = "https://storage.googleapis.com/kagglesdsdata/datasets/463494/941394/Shakespeare_01.txt?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240124%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240124T015418Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=5d0da122a3e4c7a356b8d4940847e96cc92e695cbd330044d29cc7a752d7810001753662bacdf25b21bfe4d0d3ff633a4b8f3f1ef23bd68afbdbecaa210e4b5f6c6a4614b940fcdc0beded8079a2829c3c38d860b46080222d9262db2f0f1954678287bcbafdf881e757ac9c106c1f271e63859ee5a790d3efa4a678933f11905932e44d61107699118e2bec17cdc8126c8437d422e7239000b9a32c66ee2ad253122c4be55e0ad73265a4fc55b6a3d453ab4bf1e2b82a55b1379bf33818a2cea0acf439669257e3058c7193047714e74a196efeb2eff7fddbed73a14dd22ba99f029f0dca5f55db2796fec7b6e67f33786af37d1781769f75946bc9f791323e"
     url_excel = "https://github.com/mikeygman11/Baseball-Statistics/raw/master/2019mlb.xlsx"
     url_csv = "https://query.data.world/s/352fjx2iiw5427wzmelxeky6fql6uh?dws=00000"
-    url_json = "https://storage.googleapis.com/kagglesdsdata/datasets/496274/945103/StadiumsFull.json?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240124%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240124T021508Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=71e775d54b626f4f795aeebb70198e1dad862cb099ea8401a2c8657c507a3040d847a2288ba19b9d37adc9183e978c993c7cf4f7c355ca633c08dcd0d8fa655dc37b8b82a3cf06c23aee21e0452af773f2b2d0fcf19465f682b63d3bb664c88a9ef121685bf163b8291150848dace19a7aa2ecc2507e401d18e81c316a7c7acd61a7ceecbfd3162058bdf3f6065bfc6154e59e7059e38da497d0e638398a34544eff4b32f3255cea82868fa9b3f5fe394abe6ba914489329451dafc667db827161905aa497fc971544cb2b4bf283c9c6e86c7c05bbdb02d00a4be86c127da101aad3a01c1ae6772a1d2518774a284b2b1da3dc3c8398770d749b3a000a348ba0"
+    url_json = "https://dog.ceo/api/breeds/image/random"
     
     # folder names to write data to
     txt_folder_name = "data-txt"
@@ -116,6 +183,11 @@ def main():
     fetch_and_write_excel_data(excel_folder_name, excel_filename, url_excel)
     fetch_and_write_csv_data(csv_folder_name, csv_filename, url_csv)
     fetch_and_write_json_data(json_folder_name, json_filename, url_json)
+
+    process_txt_file(txt_folder_name,'data.txt', 'results_txt.txt')
+    process_csv_file(csv_folder_name,'data.csv', 'results_csv.txt')
+    process_excel_file(excel_folder_name,'data.xls', 'results_xls.txt')
+    process_json_file(json_folder_name,'data.json', 'results_json.txt')
     
 # conditional execution
 if __name__ == "__main__":
